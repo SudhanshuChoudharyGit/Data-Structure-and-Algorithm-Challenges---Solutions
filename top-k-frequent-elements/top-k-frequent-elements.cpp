091893d1de5205dfd1de56346686959337f74305
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-04-19 09:11:24 +0530

    Time: 18 ms (56.56%) | Memory: 13.7 MB (77.46%) - LeetSync

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -3,7 +3,8 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         //bucket sort
         //unordered_map -> filled in vector of vectors with index as frequency
-        vector<int> ans;
+        //time complexity: O(n)
+        /*vector<int> ans;
         unordered_map<int,int> mp;
         for(int n: nums)
         {
@@ -21,6 +22,25 @@ public:
             {
                 ans.push_back(bucket[i][j]);
                 if(ans.size()==k) return ans;
+            }
+        }
+        return ans;*/
+
+        //approach2: not needing this much space (priority queue)
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        for(int n: nums)
+        {
+            mp[n]++;
+        }
+        priority_queue<pair<int,int>> pq;
+        for(auto kv: mp)
+        {
+            pq.push(make_pair(kv.second,kv.first));
+            if(pq.size()>mp.size()-k)
+            {
+                ans.push_back(pq.top().second);
+                pq.pop();
             }
         }
         return ans;
