bc27c3369d5de0c10b86739b7f50b1872c86b913
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-04-29 12:53:15 +0530

    Time: 5 ms (67.20%) | Memory: 9.1 MB (29.19%) - LeetSync

---
+++ clone-graph/clone-graph.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    Node* dfs(Node* node,unordered_map<Node*,Node*>& mp) {
+
+        if(mp.find(node)==mp.end())
+        {
+            mp[node] = new Node(node->val);
+
+            for(auto adjele : node->neighbors)
+            {
+                (mp[node]->neighbors).push_back(dfs(adjele,mp));
+            }
+        }
+
+        return mp[node];
+    }
+
+
+    Node* cloneGraph(Node* node) {
+        
+        if(node==NULL) return NULL;
+        unordered_map<Node*,Node*> mp;
+        return dfs(node,mp);
+    }
+};
