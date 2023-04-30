de8a949f1ccaf7e13d5c7a81921dbd0d9fb8a2eb
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-04-30 14:12:34 +0530

    Time: 43 ms (72.40%) | Memory: 17.5 MB (86.87%) - LeetSync

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,86 +1,58 @@
 class Solution {
 public:
 
-    struct hashFunction
-    {
-    size_t operator()(const pair<int , 
-                        int> &x) const
-    {
-        return x.first ^ x.second;
-    }
-    };
-
     bool isSafe(int i,int j,int n,int m)
     {
         if(i>=0 && i<n && j>=0 && j<m) return true;
         else return false;
     }
 
-    void dfs(vector<vector<int>>& heights,int n,int m,vector<vector<bool>>& vis,unordered_set<pair<int,int>,hashFunction>& s,int i,int j,vector<vector<int>>& ans) {
+    void dfs(vector<vector<int>>& heights,int n,int m,vector<vector<bool>>& vis,int i,int j) {
+        
         vis[i][j]=true;
-        if(s.find({i,j})!=s.end())
-        {
-            vector<int> v;
-            v.push_back(i);
-            v.push_back(j);
-            ans.push_back(v);
-        } 
-        else s.insert({i,j});
+
         int val=heights[i][j];
 
-        if(isSafe(i+1,j,n,m) && !vis[i+1][j] && heights[i+1][j]>=val) dfs(heights,n,m,vis,s,i+1,j,ans);
-        if(isSafe(i-1,j,n,m) && !vis[i-1][j] && heights[i-1][j]>=val) dfs(heights,n,m,vis,s,i-1,j,ans);
-        if(isSafe(i,j+1,n,m) && !vis[i][j+1] && heights[i][j+1]>=val) dfs(heights,n,m,vis,s,i,j+1,ans);
-        if(isSafe(i,j-1,n,m) && !vis[i][j-1] && heights[i][j-1]>=val) dfs(heights,n,m,vis,s,i,j-1,ans);
+        if(isSafe(i+1,j,n,m) && !vis[i+1][j] && heights[i+1][j]>=val) dfs(heights,n,m,vis,i+1,j);
+        if(isSafe(i-1,j,n,m) && !vis[i-1][j] && heights[i-1][j]>=val) dfs(heights,n,m,vis,i-1,j);
+        if(isSafe(i,j+1,n,m) && !vis[i][j+1] && heights[i][j+1]>=val) dfs(heights,n,m,vis,i,j+1);
+        if(isSafe(i,j-1,n,m) && !vis[i][j-1] && heights[i][j-1]>=val) dfs(heights,n,m,vis,i,j-1);
 
     }
 
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        //For pacific
-        //dfs from edges (2 for each ocean)
-        //mark visited
-        //check neighbors -  if not visited
-        //then check if greater or equal to current
-        //then call dfs on them in turn
-        //store all in un unordered_map
+        //do dfs from edges of both atlantic and pacific
+        //have a diff vis 2D array for for of them: keeps track of elements which can send water to them respectively
+        //finally for the indexes for which both vis array and true are the answer
 
-        //do the same for atlantis but if a position is in map -> add to answer
-
-        //for pacific
 
         int n=heights.size();
         int m=heights[0].size();
 
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        vector<vector<bool>> pacific(n,vector<bool>(m,false));
+        vector<vector<bool>> atlantic(n,vector<bool>(m,false));
 
         vector<vector<int>> ans;
 
-        unordered_set<pair<int,int>,hashFunction> s;
 
         for(int i=0;i<n;i++)
         {
-            if(!vis[i][0]) dfs(heights,n,m,vis,s,i,0,ans);
+            dfs(heights,n,m,pacific,i,0);
+            dfs(heights,n,m,atlantic,i,m-1);
         }
 
-        for(int j=1;j<m;j++)
+        for(int j=0;j<m;j++)
         {
-            if(!vis[0][j]) dfs(heights,n,m,vis,s,0,j,ans);
+            dfs(heights,n,m,pacific,0,j);
+            dfs(heights,n,m,atlantic,n-1,j);
         }
 
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++) vis[i][j]=false;
-        }
-
-        //for atlatic
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i][m-1]) dfs(heights,n,m,vis,s,i,m-1,ans);
-        }
-
-        for(int j=0;j<m-1;j++)
-        {
-            if(!vis[n-1][j]) dfs(heights,n,m,vis,s,n-1,j,ans);
+            for(int j=0;j<m;j++)
+            {
+                if(pacific[i][j] && atlantic[i][j]) ans.push_back({i,j});
+            }
         }
 
         return ans;
