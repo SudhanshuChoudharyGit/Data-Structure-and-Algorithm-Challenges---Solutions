326c9807928c9fe6f6b8439e0d7262c1621f07ad
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-05-09 09:11:23 +0530

    Time: 16 ms (67.43%) | Memory: 13.6 MB (90.25%) - LeetSync

--- top-k-frequent-elements/top-k-frequent-elements.cpp
+++ top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        //bucket sort
-        //unordered_map -> filled in vector of vectors with index as frequency
-        //time complexity: O(n)
-        /*vector<int> ans;
-        unordered_map<int,int> mp;
-        for(int n: nums)
-        {
-            mp[n]++;
-        }
-        vector<vector<int>> bucket(nums.size()+1);
-        for(auto kv: mp)
-        {
-            bucket[kv.second].push_back(kv.first);
-        }
-
-        for(int i=bucket.size()-1;i>=0;i--)
-        {
-            for(int j=0;j<bucket[i].size();j++)
-            {
-                ans.push_back(bucket[i][j]);
-                if(ans.size()==k) return ans;
-            }
-        }
-        return ans;*/
-
-        //approach2: not needing this much space (priority queue)
+        //map (for frequency)and priority queue(for holding top k frequency elements)
         vector<int> ans;
-        unordered_map<int,int> mp;
-        for(int n: nums)
+        unordered_map<int,int> count;
+        for(int element : nums)
         {
-            mp[n]++;
+            count[element]++;
         }
+
         priority_queue<pair<int,int>> pq;
-        for(auto kv: mp)
+        for(auto it : count)
         {
-            pq.push(make_pair(kv.second,kv.first));
-            if(pq.size()>mp.size()-k)
+            pq.push(make_pair(it.second,it.first));
+            if(pq.size()>count.size()-k)
             {
                 ans.push_back(pq.top().second);
+                if(ans.size()==k) return ans;
                 pq.pop();
             }
         }
