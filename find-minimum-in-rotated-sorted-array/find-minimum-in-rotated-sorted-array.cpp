e7e7854ed1e904c986d0f886135c87fa8459ca72
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-05-19 22:44:37 +0530

    Time: 3 ms (78.49%) | Memory: 10.2 MB (70.01%) - LeetSync

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -3,66 +3,20 @@ public:
     int findMin(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        int l=0;
-        int r=n-1;
+        int l,r;
+        l=0;
+        r=n-1;
         while(l<r)
         {
-            int m=(r-l)/2+l;
-            if(r==l+1 && nums[r]<nums[l]) return nums[r];
-            if(nums[l]<=nums[m] && nums[m]<nums[r]) break;
-            else if(nums[l]<nums[m]) l=m;
-            else r=m;
+            int m=(r+l)/2;
+            if(m>0 && nums[m]<nums[m-1]) return nums[m];
+            if(nums[l]<=nums[m])
+            {
+                if(nums[m]<nums[r]) return nums[l];
+                l=m+1;
+            }
+            else r=m-1;
         }
         return nums[l];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        int n=nums.size();
-        if(n==1) return nums[0];
-        int l=0;
-        int r=n-1;
-        while(l<=r)
-        {
-            int mid=l+(r-l)/2;
-            if(mid>0 && nums[mid]<nums[mid-1]) return nums[mid];
-            else if(nums[mid]>=nums[l])
-            {
-                if(nums[mid]<nums[r]) return nums[l];
-                else l=mid+1;
-            }
-            else r=mid-1;
-        }
-        return nums[0];
-        */
     }
 };
