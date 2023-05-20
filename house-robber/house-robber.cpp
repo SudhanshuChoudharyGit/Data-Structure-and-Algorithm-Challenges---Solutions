eabc39f9e7a28303933ba4773b536fcce96200cd
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-05-20 17:53:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.81%) - LeetSync

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -1,72 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        int max_money = (n==0?0:nums[0]);
+        int last=0;
         int second_last=0;
-        int last=max_money;
-        for(int i=1;i<n;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            max_money=max(last,second_last+nums[i]);
+            int curr_max=max(last,second_last+nums[i]);
             second_last=last;
-            last=max_money;
+            last=curr_max;
         }
-        return max_money;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*vector<int> dp(n+1,0);
-        dp[1]=nums[0];
-        for(int i=2;i<=n;i++)
-        {
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
-        }
-        return dp[n];
-        */
-        /*
-        int prev=nums[0];
-        int ans=prev;
-        int bprev=0;
-        int i=1;
-        while(i<n)
-        {
-            ans=max(prev,nums[i]+bprev);
-            bprev=prev;
-            prev=ans;
-            i++;
-        }
-        return ans;
-        */
+        return last;
     }
 };
