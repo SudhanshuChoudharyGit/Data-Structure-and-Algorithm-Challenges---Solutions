7b642f735bb8c9711f40c9d02da8d8e9450fcfec
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-05-21 14:23:39 +0530

    Time: 4 ms (97.51%) | Memory: 10.4 MB (88.87%) - LeetSync

--- longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
+
+    int binarysearchfirstgreater(vector<int>& sub,int target) {
+        int ans=0;
+        int l=0;
+        int r=sub.size()-1;
+        while(l<=r)
+        {
+            int m = (r-l)/2 + l;
+            if(sub[m]>=target) 
+            {
+                ans=m;
+                r=m-1;
+            }
+            else l=m+1;
+        }
+        return ans;
+    }
+
     int lengthOfLIS(vector<int>& nums) {
+
+        //dynamic programming - classic
+        //the length of longest increasing subsequence ending at index i would be 
+        //maximum amoung the LIS of previous indexs where values at those index j is 
+        //smaller than value at i
+        /*
         int n = nums.size();
         int maxlength=1;
         vector<int> dp(n,1);
@@ -16,5 +40,20 @@ public:
             }
         }
         return maxlength;
+        */
+
+        //Greedy with binary search
+        vector<int> sub;
+        sub.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]>sub[sub.size()-1]) sub.push_back(nums[i]);
+            else
+            {
+                int index = binarysearchfirstgreater(sub,nums[i]);
+                sub[index]=nums[i];
+            }
+        }
+        return sub.size();
     }
 };
