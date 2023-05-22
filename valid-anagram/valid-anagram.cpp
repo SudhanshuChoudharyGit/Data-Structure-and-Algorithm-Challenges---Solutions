63667351ce12dad9e5c1ded73a0034b8c076a1d2
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-05-22 18:21:25 +0530

    Time: 12 ms (54.83%) | Memory: 7.1 MB (97.99%) - LeetSync

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,8 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        //sorting
+        //T = O(nlongn) S = O(1)
+        /*
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         return (s==t);
+        */
+
+        //freq array - if needed unicode - not good
+
+        //map
+        unordered_map<char,int> mp;
+        for(char c : s) mp[c]++;
+
+        for(char c : t)
+        {
+            if(mp.count(c)==0) return false;
+            mp[c]--;
+        }
+
+        for(auto it : mp)
+        {
+            if(it.second!=0) return false;
+        }
+        return true;
+
     }
 };
