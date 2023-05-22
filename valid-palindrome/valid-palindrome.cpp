ed3331f8adec1cf2d2f1eacefc636981cdf2cb75
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-05-22 16:47:11 +0530

    Time: 7 ms (48.75%) | Memory: 7.6 MB (44.18%) - LeetSync

---
+++ valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str="";
+        for(char c : s)
+        {
+            if(c>='a' && c<='z' || c>='A' && c<='Z' || c>='0' && c<='9')
+            {
+                str+=tolower(c);
+            }
+        }
+        int n=str.length();
+        for(int i=0;i<n/2;i++)
+        {
+            if(str[i]!=str[n-i-1]) return false;
+        }
+        return true;
+    }
+};
