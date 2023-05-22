5a2406fc54ec1d01ab5650731661b7ea0fe4bcce
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-05-22 18:48:15 +0530

    Time: 42 ms (49.49%) | Memory: 19.3 MB (90.23%) - LeetSync

---
+++ group-anagrams/group-anagrams.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_multimap<string,int> mp;
+        vector<vector<string>> ans;
+        for(int i=0;i<strs.size();i++)
+        {
+            string s = strs[i];
+            sort(s.begin(),s.end());
+            mp.insert({s,i});
+        }
+        auto it = mp.begin();
+        while(it!=mp.end())
+        {
+            int x = mp.count(it->first);
+            vector<string> anagrams;
+            while(x--)
+            {
+                anagrams.push_back(strs[it->second]);
+                it++;
+            }
+            ans.push_back(anagrams);
+        }
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        multimap<string,int> mp;
+        for(int i=0;i<strs.size();i++)
+        {
+            string s=strs[i];
+            sort(s.begin(),s.end());
+            mp.insert({s,i});
+        }
+
+        vector<vector<string>> ans;
+        for(auto itr=mp.begin();itr!=mp.end();)
+        {
+            string s=itr->first;
+            vector<string> v;
+            int x=mp.count(s);
+            while(x--)
+            {
+                v.push_back(strs[itr->second]);
+                ++itr;
+            }
+            ans.push_back(v);
+        }
+        return ans;
+        */
+    }
+};
