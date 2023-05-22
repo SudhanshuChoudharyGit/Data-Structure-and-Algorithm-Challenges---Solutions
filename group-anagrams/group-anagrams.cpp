1865686efa69dfd46444f0073f12e84d4e7849b0
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-05-22 18:57:12 +0530

    Time: 28 ms (95.43%) | Memory: 20.7 MB (43.40%) - LeetSync

--- group-anagrams/group-anagrams.cpp
+++ group-anagrams/group-anagrams.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //store indexes in multimap
+        /*
         unordered_multimap<string,int> mp;
         vector<vector<string>> ans;
         for(int i=0;i<strs.size();i++)
@@ -20,6 +22,22 @@ public:
                 it++;
             }
             ans.push_back(anagrams);
+        }
+        return ans;
+        */
+
+        //Store strings in multimap
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mp;
+        for(string str : strs)
+        {
+            string s = str;
+            sort(s.begin(),s.end());
+            mp[s].push_back(str);
+        }
+        for(auto it : mp)
+        {
+            ans.push_back(it.second);
         }
         return ans;
 
