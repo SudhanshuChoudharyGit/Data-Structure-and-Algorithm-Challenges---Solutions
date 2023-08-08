6fd92f8e3329b872dcbc4bc82666e7167a7e88d3
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-08 20:17:38 +0530

    Time: 7 ms (69.40%) | Memory: 9 MB (71.46%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -55,42 +55,67 @@ public:
 
 
      //approach - backtracking - memoization
-    bool isPrefix(string source,string target) {
-        int i=0;
-        for(char c : source)
-        {
-            if(c!=target[i]) return false;
-            i++;
-        }
-        return true;
-    }
+    // bool isPrefix(string source,string target) {
+    //     int i=0;
+    //     for(char c : source)
+    //     {
+    //         if(c!=target[i]) return false;
+    //         i++;
+    //     }
+    //     return true;
+    // }
 
-    bool backtrack(string s,vector<string>& wordDict,string curr,unordered_map<string,bool>& mp) {
-        if(curr == s) return true;
-        for(auto word : wordDict)
+    // bool backtrack(string s,vector<string>& wordDict,string curr,unordered_map<string,bool>& mp) {
+    //     if(curr == s) return true;
+    //     for(auto word : wordDict)
+    //     {
+    //         if(!isPrefix(curr+word,s)) continue;
+    //         if(mp.find(curr+word)!=mp.end())
+    //         {
+    //             if(mp[curr+word]) return true;
+    //         }
+    //         else {
+    //             if(backtrack(s,wordDict,curr+word,mp))
+    //             {
+    //                 mp[curr+word]=true;
+    //                 return true;
+    //             }
+    //             else mp[curr+word]=false;
+    //         }   
+    //     }
+    //     return false;
+    // }
+
+    // bool wordBreak(string s, vector<string>& wordDict) {
+    //     string curr="";
+    //     unordered_map<string,bool> mp;
+    //     return backtrack(s,wordDict,curr,mp);
+    // }
+
+// Approach - backtrack with memoization but instead of building number we will be reducing numner as soon as prefix is found
+
+    bool backtrack(string s,vector<string>& wordDict,int start_index,vector<int>& memo) {
+        if(start_index == s.length()) return true;
+        if(memo[start_index]!=-1) return (memo[start_index]==1);
+        for(string word : wordDict)
         {
-            if(!isPrefix(curr+word,s)) continue;
-            if(mp.find(curr+word)!=mp.end())
+            if(word==s.substr(start_index,word.length()))
             {
-                if(mp[curr+word]) return true;
-            }
-            else {
-                if(backtrack(s,wordDict,curr+word,mp))
+                if(backtrack(s,wordDict,start_index+word.length(),memo))
                 {
-                    mp[curr+word]=true;
+                    memo[start_index]=1;
                     return true;
                 }
-                else mp[curr+word]=false;
-            }   
+            }
         }
+        memo[start_index]=0;
         return false;
     }
 
-    bool wordBreak(string s, vector<string>& wordDict) {
-        string curr="";
-        unordered_map<string,bool> mp;
-        return backtrack(s,wordDict,curr,mp);
-    }
 
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<int> memo(s.length(),-1);
+        return backtrack(s,wordDict,0,memo);
+    }
 
 };
