ad2122992c4c69e60c4db7e04c58e5bb243299ba
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-08 10:00:17 +0530

    Time: 5 ms (29.36%) | Memory: 7.5 MB (81.22%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -60,25 +60,47 @@ public:
 
 
     // Approach 3
-        void swapAndSelect(vector<int>& nums,vector<vector<int>>& ans,int index)
-    {
-        if(index==nums.size())
+    //     void swapAndSelect(vector<int>& nums,vector<vector<int>>& ans,int index)
+    // {
+    //     if(index==nums.size())
+    //     {
+    //         ans.push_back(nums);
+    //         return;
+    //     }
+
+    //     for(int i=index;i<nums.size();i++)
+    //     {
+    //         swap(nums[i],nums[index]);
+    //         swapAndSelect(nums,ans,index+1);
+    //         swap(nums[i],nums[index]);
+    //     }
+    // }
+
+    // vector<vector<int>> permute(vector<int>& nums) {
+    //     vector<vector<int>> ans;
+    //     swapAndSelect(nums,ans,0);
+    //     return ans;
+    // }
+
+    // Appraoch 4
+
+    void backtrack(vector<vector<int>>& ans,vector<int>& nums,int index) {
+        if(index == nums.size())
         {
             ans.push_back(nums);
             return;
         }
-
         for(int i=index;i<nums.size();i++)
         {
             swap(nums[i],nums[index]);
-            swapAndSelect(nums,ans,index+1);
+            backtrack(ans,nums,index+1);
             swap(nums[i],nums[index]);
         }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        swapAndSelect(nums,ans,0);
+        backtrack(ans,nums,0);
         return ans;
     }
 
