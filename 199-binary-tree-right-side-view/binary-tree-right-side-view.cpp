36f06842b0e33e0f32be7a89b1a899d6d3fb02cb
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-09 19:53:42 +0530

    Time: 3 ms (70.24%) | Memory: 12.1 MB (34.48%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -18,14 +18,14 @@ public:
         q.push(root);
         while(!q.empty())
         {
+            ans.push_back(q.front()->val);
             int levelSize = q.size();
             while(levelSize--)
             {
                 TreeNode* node = q.front();
                 q.pop();
-                if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
-                if(levelSize==0) ans.push_back(node->val);
+                if(node->left) q.push(node->left);
             }
         }
         return ans;
