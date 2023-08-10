5929668a44f8fcf65c37c3996c21bf8510dd76fc
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-10 08:57:32 +0530

    Time: 49 ms (28.38%) | Memory: 21.8 MB (11.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -23,37 +23,60 @@ public:
         
     // }
 
-    vector<pair<int,int>> getNeighbors(vector<vector<char>>& grid,int r,int c){
-        vector<pair<int,int>> neighbors;
-        int del_row[4]={-1,0,0,1};
-        int del_col[4]={0,-1,1,0};
-        for(int i=0;i<4;i++)
-        {
-            int row= r+del_row[i];
-            int col= c+del_col[i];
-            if(row>=0 && col>=0 && row<grid.size() && col<grid[0].size())
-            {
-                neighbors.push_back({row,col});
-            }
-        }
-        return neighbors;
-    }
+    // vector<pair<int,int>> getNeighbors(vector<vector<char>>& grid,int r,int c){
+    //     vector<pair<int,int>> neighbors;
+    //     int del_row[4]={-1,0,0,1};
+    //     int del_col[4]={0,-1,1,0};
+    //     for(int i=0;i<4;i++)
+    //     {
+    //         int row= r+del_row[i];
+    //         int col= c+del_col[i];
+    //         if(row>=0 && col>=0 && row<grid.size() && col<grid[0].size())
+    //         {
+    //             neighbors.push_back({row,col});
+    //         }
+    //     }
+    //     return neighbors;
+    // }
+
+    // void bfs(vector<vector<char>>& grid,vector<vector<bool>>& vis,int i,int j) {
+    //     queue<pair<int,int>> q;
+    //     q.push({i,j});
+    //     vis[i][j]=true;
+    //     while(!q.empty())
+    //     {
+    //         pair<int,int> pa = q.front();
+    //         q.pop();
+    //         vector<pair<int,int>> neighbors = getNeighbors(grid,pa.first,pa.second);
+    //         for(auto p : neighbors)
+    //         {
+    //             if(!vis[p.first][p.second] && grid[p.first][p.second]=='1')
+    //             {
+    //                 vis[p.first][p.second]=true;
+    //                 q.push(p);
+    //             }
+    //         }
+    //     }
+    // }
 
     void bfs(vector<vector<char>>& grid,vector<vector<bool>>& vis,int i,int j) {
         queue<pair<int,int>> q;
         q.push({i,j});
         vis[i][j]=true;
+        vector<vector<int>> coordinates={{-1,0},{0,-1},{0,1},{1,0}};
         while(!q.empty())
         {
-            pair<int,int> pa = q.front();
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
-            vector<pair<int,int>> neighbors = getNeighbors(grid,pa.first,pa.second);
-            for(auto p : neighbors)
+            for(int i=0;i<4;i++)
             {
-                if(!vis[p.first][p.second] && grid[p.first][p.second]=='1')
+                int xn = x + coordinates[i][0];
+                int yn = y + coordinates[i][1];
+                if(xn>=0 && yn>=0 && xn<grid.size() && yn<grid[0].size() && !vis[xn][yn] && grid[xn][yn]=='1')
                 {
-                    vis[p.first][p.second]=true;
-                    q.push(p);
+                    q.push({xn,yn});
+                    vis[xn][yn]=true;
                 }
             }
         }
@@ -77,6 +100,9 @@ public:
         }
         return ans;
     }
+
+
+
 
 
 };
