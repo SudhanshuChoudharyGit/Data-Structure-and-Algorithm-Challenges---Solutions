0b6052b712725f146a1271fa6887a2348ee2d841
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-11 08:50:45 +0530

    Time: 136 ms (88.31%) | Memory: 34.7 MB (70.46%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -3,7 +3,6 @@ public:
     //bfs
     int openLock(vector<string>& deadends, string target) {
         if(target=="0000") return 0;
-        vector<char> num = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         queue<string> q;
         unordered_set<string> vis;
         q.push("0000");
@@ -21,14 +20,19 @@ public:
                 string slot = q.front();
                 if(slot==target) return step;
                 q.pop();
+                string stepup,stepdown;
+                
                 for(int index=0;index<4;index++)
-                {   
-                    string stepup;
-                    if(slot[index]=='9') stepup = slot.substr(0,index) + '0' + slot.substr(index+1);
-                    else stepup = slot.substr(0,index) + num[slot[index]-'0'+1] + slot.substr(index+1);
-                    string stepdown;
-                    if(slot[index]=='0') stepdown = slot.substr(0,index) + '9' + slot.substr(index+1);
-                    else stepdown = slot.substr(0,index) + num[slot[index]-'0'-1] + slot.substr(index+1);
+                { 
+                    stepup = stepdown = slot;
+                    char upchar = (slot[index]=='9')?'0':slot[index]+1;
+                    char downchar = (slot[index]=='0')?'9':slot[index]-1; 
+                    stepup[index]=upchar;
+                    stepdown[index]=downchar;
+                    // if(slot[index]=='9') stepup = slot.substr(0,index) + '0' + slot.substr(index+1);
+                    // else stepup = slot.substr(0,index) + num[slot[index]-'0'+1] + slot.substr(index+1);
+                    // if(slot[index]=='0') stepdown = slot.substr(0,index) + '9' + slot.substr(index+1);
+                    // else stepdown = slot.substr(0,index) + num[slot[index]-'0'-1] + slot.substr(index+1);
                     if(vis.count(stepup)==0)
                     {
                         vis.insert(stepup);
