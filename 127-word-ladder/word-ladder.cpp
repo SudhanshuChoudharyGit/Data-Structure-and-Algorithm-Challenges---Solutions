b730b9fd0952c4fe7237aa6aee5c450409458398
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-11 09:53:19 +0530

    Time: 93 ms (73.61%) | Memory: 15.5 MB (40.61%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
 
-    vector<string> adjacentWords(unordered_set<string>& wordSet, string curr)
-    {
-        vector<string> ans;
-        for(auto word : wordSet)
-        {
-            int diff=0;
-            for(int i=0;i<curr.length();i++)
-            {
-                if(curr[i]!=word[i]) diff++;
-            }
-            if(diff==1) ans.push_back(word);
-        }
-        return ans;
-    }
+    // vector<string> adjacentWords(unordered_set<string>& wordSet, string curr)
+    // {
+    //     vector<string> ans;
+    //     for(auto word : wordSet)
+    //     {
+    //         int diff=0;
+    //         for(int i=0;i<curr.length();i++)
+    //         {
+    //             if(curr[i]!=word[i]) diff++;
+    //         }
+    //         if(diff==1) ans.push_back(word);
+    //     }
+    //     return ans;
+    // }
 
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         if(beginWord==endWord) return 0;
@@ -33,11 +33,28 @@ public:
                 string curr = q.front();
                 q.pop();
                 if(curr == endWord) return step;
-                for(auto word : adjacentWords(wordSet,curr))
+                for(int i=0;i<curr.length();i++)
                 {
-                    wordSet.erase(word);
-                    q.push(word);
-                } 
+                    char ogchar= curr[i];
+                    for(int j=0;j<26;j++)
+                    {
+                        if(('a'+j)!=ogchar)
+                        {
+                            string newWord = curr;
+                            newWord[i]='a'+j;
+                            if(wordSet.count(newWord)!=0)
+                            {
+                                q.push(newWord);
+                                wordSet.erase(newWord);
+                            }
+                        }
+                    }
+                }
+                // for(auto word : adjacentWords(wordSet,curr))
+                // {
+                //     wordSet.erase(word);
+                //     q.push(word);
+                // } 
             }
             step++;
         }
