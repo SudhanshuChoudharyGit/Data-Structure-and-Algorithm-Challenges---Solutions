dfdf1976c573a9413bce3a0b2d99309ac08dee7e
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-11 09:46:36 +0530

    Time: 732 ms (23.84%) | Memory: 16.8 MB (27.54%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    vector<string> adjacentWords(unordered_set<string>& wordSet, string curr)
+    {
+        vector<string> ans;
+        for(auto word : wordSet)
+        {
+            int diff=0;
+            for(int i=0;i<curr.length();i++)
+            {
+                if(curr[i]!=word[i]) diff++;
+            }
+            if(diff==1) ans.push_back(word);
+        }
+        return ans;
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        if(beginWord==endWord) return 0;
+        unordered_set<string> wordSet;
+        for(string word : wordList) wordSet.insert(word);
+        if(wordSet.count(endWord)==0) return 0;
+        queue<string> q;
+        q.push(beginWord);
+        if(wordSet.count(beginWord)!=0) wordSet.erase(beginWord);
+        int step=1;
+        while(!q.empty())
+        {
+            int stepSize = q.size();
+            while(stepSize--)
+            {
+                string curr = q.front();
+                q.pop();
+                if(curr == endWord) return step;
+                for(auto word : adjacentWords(wordSet,curr))
+                {
+                    wordSet.erase(word);
+                    q.push(word);
+                } 
+            }
+            step++;
+        }
+        return 0;
+    }
+};
