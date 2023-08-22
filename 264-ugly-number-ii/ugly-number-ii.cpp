b6457f2a4bb1da50fce6bd296742547e444a04e3
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-22 18:43:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (87.78%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,41 +1,51 @@
 class Solution {
 public:
 
-    //ugly numbers are just multiples of 2,3 and 5
-    //thus producing them is just choosing to multiply with 2,3 or 5 at a time
-    //starting with 1, it will look like this
-    //     1
-    // /   |   
-    // 2   3   5
-    // / | 
-    // 4 6 10
-
+    //using pointers
     int nthUglyNumber(int n) {
-        priority_queue<long,vector<long>,greater<>> pq;
-        unordered_set<long> used;
-        pq.push(1);
-        used.insert(1);
-        n--;
-        while(n--)
+        int p2,p3,p5;
+        p2=p3=p5=0;
+        vector<int> ugly(n);
+        ugly[0]=1;
+        int num = 1;
+        for(int i=1;i<n;i++)
         {
-            int x = pq.top();
-            pq.pop();
-            if(used.count(x*2l)==0)
-            {
-                pq.push(x*2l);
-                used.insert(x*2l);
-            } 
-            if(used.count(x*3l)==0)
-            {
-                pq.push(x*3l);
-                used.insert(x*3l);
-            } 
-            if(used.count(x*5l)==0)
-            {
-                pq.push(x*5l);
-                used.insert(x*5l);
-            } 
+            num = min(ugly[p2]*2,min(ugly[p3]*3,ugly[p5]*5));
+            ugly[i]=num;
+            if(num == ugly[p2]*2) p2++;
+            if(num == ugly[p3]*3) p3++;
+            if(num == ugly[p5]*5) p5++;
         }
-        return pq.top();
+        return num;
     }
+
+    //using priority queue
+    // int nthUglyNumber(int n) {
+    //     priority_queue<long,vector<long>,greater<>> pq;
+    //     unordered_set<long> used;
+    //     pq.push(1);
+    //     used.insert(1);
+    //     n--;
+    //     while(n--)
+    //     {
+    //         int x = pq.top();
+    //         pq.pop();
+    //         if(used.count(x*2l)==0)
+    //         {
+    //             pq.push(x*2l);
+    //             used.insert(x*2l);
+    //         } 
+    //         if(used.count(x*3l)==0)
+    //         {
+    //             pq.push(x*3l);
+    //             used.insert(x*3l);
+    //         } 
+    //         if(used.count(x*5l)==0)
+    //         {
+    //             pq.push(x*5l);
+    //             used.insert(x*5l);
+    //         } 
+    //     }
+    //     return pq.top();
+    // }
 };
