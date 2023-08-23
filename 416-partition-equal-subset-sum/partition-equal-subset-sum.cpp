1f8461270f0263e9a8b020bb32cc8fd2bab2f3a7
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-23 19:02:31 +0530

    Time: 438 ms (25.69%) | Memory: 9.8 MB (90.75%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
 
+    bool canPartition(vector<int>& nums) {
+        int totalsum=0;
+
+        for(auto num : nums) totalsum+=num;
+
+        if(totalsum%2!=0) return false;
+
+        int targetNum = totalsum/2;
+
+        vector<vector<bool>> dp(2,vector<bool>(targetNum+1,false));
+
+        dp[0][0]=true;
+
+        for(auto num : nums)
+        {
+            for(int target=0;target<=targetNum;target++)
+            {
+                dp[1][target] = dp[0][target];
+
+                if(target>=num)
+                dp[1][target] = dp[1][target] || dp[0][target-num];
+            }
+
+            for(int target=0;target<=targetNum;target++)
+            {
+                if(dp[1][target]) dp[0][target]=true;
+            }
+        }
+
+        return dp[0][targetNum];
+    }
+
+
+
+
+
+
+
+
+
+// Backtracking + memo = top down dp
+/*
+
     int backtrack(vector<int>& nums, int total, int currsum,int index, vector<vector<int>>& memo) {
         if(memo[currsum][index]!=-1) return memo[currsum][index];
 
@@ -34,4 +77,5 @@ public:
         int ans = backtrack(nums, total, 0, 0, memo);
         return ans == 1;
     }
+*/
 };
