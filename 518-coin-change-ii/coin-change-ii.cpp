8377e3ffeb021ad51dcaad282cf51b6d3f412a6e
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-24 19:32:14 +0530

    Time: 8 ms (82.21%) | Memory: 7.2 MB (77.65%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
 
+    //bottom up dp
+    //dp[sum]=sum of dp[sum-coins[i]] for all i's possible where sum>=coins[i] 
+    //if dp[sum-coins[i]]!=0
+    int change(int amount, vector<int>& coins) {
+        sort(coins.begin(),coins.end());
+        vector<int> dp(amount+1,0);
+        dp[0]=1;
+        for(auto coin: coins)
+        {
+            for(int sum=coin;sum<=amount;sum++)
+            {
+                dp[sum]+=dp[sum-coin];
+            }
+        }
+        return dp[amount];
+    }
+
+
+
+
+/*
+    //backtrack + memoization: top -down dp
     int backtrack(int amount,vector<int>& coins,int sum,int index,vector<vector<int>>& dp) {
         if(dp[sum][index]!=-1) return dp[sum][index];
         if(sum==amount) return 1;
@@ -19,4 +41,5 @@ public:
         vector<vector<int>> dp(amount+1,vector<int>(coins.size(),-1));
         return backtrack(amount,coins,0,0,dp);
     }
+*/
 };
