02b299d0ec515be1aafb6264a68effc9f781e291
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-24 10:15:05 +0530

    Time: 58 ms (90.39%) | Memory: 21.7 MB (41.63%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,111 @@
+class Solution {
+public:
+
+    long backtrack(vector<int>& coins,long amount,long sum,vector<long>& memo)
+    {
+        if(memo[sum]!=-1) return memo[sum];
+        if(amount==sum) return 0;
+        long usedCoins=INT_MAX;
+        for(int coin : coins)
+        {
+            if((long)(sum+coin)>amount) break;
+            usedCoins = min(usedCoins,backtrack(coins,amount,sum+coin,memo)+1);
+        }
+        memo[sum]=usedCoins;
+        return usedCoins;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end());
+        vector<long> memo(amount+1,-1);
+        long ans = backtrack(coins,amount,0l,memo);
+        return (ans==INT_MAX) ? -1 : (int)ans;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int coinChange(vector<int>& coins, int amount) {
+
+    //     vector<int> v(amount+1,-1);
+
+    //     sort(coins.begin(),coins.end());
+
+    //     int n=coins.size();
+
+    //     v[0]=0;
+
+    //     for(int i=1;i<=amount;i++)
+    //     {
+    //         int j=0;
+    //         int x=INT_MAX;
+    //         while(j<n && i>=coins[j])
+    //         {
+    //             if(v[i-coins[j]]!=-1)
+    //             {
+    //                 x=min(x,v[i-coins[j]]+1);
+    //             }
+    //             j++;
+    //         }
+    //         if(x!=INT_MAX) v[i]=x;
+    //     }
+
+    //     return v[amount];
+
+    // }
+/*
+    // approach - Backtracking
+    int backtrack(int amount, vector<int>& coins,vector<int>& memo) {
+        if(amount == 0) return 0;
+        if(memo[amount]!=-1) return memo[amount];
+        int ans = INT_MAX;
+        for(auto coin : coins)
+        {
+            if(coin > amount) continue;
+            int ret = backtrack(amount - coin, coins,memo);
+            if(ret!=INT_MAX)
+            {
+                ans = min(ans, ret + 1);
+                break;
+            }
+        }
+        memo[amount]=ans;
+        return ans;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end(),greater<int> ()); //descending order
+        vector<int> memo(amount+1,-1);
+        memo[0]=0;
+        int ans = backtrack(amount,coins,memo);
+        return (ans==INT_MAX) ? -1 : ans ;
+    }
+*/
+};
