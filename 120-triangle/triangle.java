01d8e6be15b53af80f25c46d2192497c1da68d18
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-28 17:24:06 +0530

    Time: 2 ms (76.07%) | Memory: 44.1 MB (42.49%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,19 +2,18 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
         
-        int[][] dp= new int[2][n];
+        int[] dp= new int[n];
 
-        for(int j=0;j<n;j++) dp[1][j]=triangle.get(n-1).get(j);
+        for(int i=0;i<n;i++) dp[i]=triangle.get(n-1).get(i);
 
         for(int level=n-2;level>=0;level--)
         {
             for(int i=0;i<=level;i++)
             {
-                dp[0][i]= Math.min(dp[1][i],dp[1][i+1])+triangle.get(level).get(i);
+                dp[i]= Math.min(dp[i],dp[i+1])+triangle.get(level).get(i);
             }
-            for(int i=0;i<=level;i++) dp[1][i]=dp[0][i];
         }
-        return dp[1][0];
+        return dp[0];
 
     }
 }
