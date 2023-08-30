8a6c554842ec5e1926d7f055b8dd1f923cca1ee9
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-08-30 10:51:15 +0530

    Time: 6 ms (87.72%) | Memory: 43.5 MB (41.46%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,4 +1,48 @@
+import java.util.*;
+
 class Solution {
+
+    public int firstEqualorGreater(Vector<Integer> v,int target){
+        int l=0;
+        int r= v.size()-1;
+
+        int ansind=0;
+
+        while(l<=r)
+        {
+            int m = l + (r-l)/2;
+            if(v.get(m)>=target)
+            {
+                ansind = m;
+                r=m-1;
+            }
+            else l=m+1;
+        }
+
+        return ansind;
+    }
+
+    //binary search
+    public int lengthOfLIS(int[] nums) {
+        Vector<Integer> v = new Vector<Integer>();
+
+        v.add(nums[0]);
+
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]>v.get(v.size()-1)) v.add(nums[i]);
+            else
+            {
+                int index = firstEqualorGreater(v,nums[i]);
+                v.set(index,nums[i]);
+            }
+        }
+
+        return v.size();
+    }
+
+    //dp
+    /*
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
@@ -18,4 +62,5 @@ class Solution {
         }
         return ans;
     }
+    */
 }
