d6a0cb1555d768183883fa923fd7dcc0d547f4e4
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-09-13 11:32:59 +0530

    Time: 4 ms (9.05%) | Memory: 44.6 MB (5.20%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public void dfs(TreeNode root,HashMap<TreeNode,Integer> map)
+    {
+        if(root==null) return;
+        map.put(root,1);
+        if(root.left!=null){
+        if(map.get(root.left)==-1) root.left=null;
+        else dfs(root.left,map);
+        }
+        if(root.right!=null){
+        if(map.get(root.right)==-1) root.right=null;
+        else dfs(root.right,map);
+        }
+    }
+
+    public void preorder(TreeNode root,HashMap<TreeNode,Integer> map,Set<Integer> st) {
+        if(root==null) return;
+        map.put(root,0);
+        if(st.contains(root.val)) map.put(root,-1);
+        preorder(root.left,map,st);
+        preorder(root.right,map,st);
+    }
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        List<TreeNode> ans = new ArrayList<>();
+        HashMap<TreeNode,Integer> map = new LinkedHashMap<>();
+        Set<Integer> st = new HashSet<>();
+        for(int x : to_delete) st.add(x);
+        preorder(root,map,st);
+        //not visited = 0
+        //visited = 1
+        //delete = -1
+        for(var entry : map.entrySet())
+        {
+            if(entry.getValue()==0) 
+            {
+                ans.add(entry.getKey());
+                dfs(entry.getKey(),map);
+            }
+        }
+        return ans;
+
+    }
+}
