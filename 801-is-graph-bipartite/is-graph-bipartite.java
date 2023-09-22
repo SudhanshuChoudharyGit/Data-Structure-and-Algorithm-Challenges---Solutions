8fe910c1d3c149ca609e2c0df33e729ae84214f1
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-09-22 09:28:44 +0530

    Time: 1 ms (65.50%) | Memory: 43.7 MB (84.09%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -3,6 +3,7 @@ class Solution {
     //visited group 1 = -1; //false
     //visited group 2 = 1;  //true
 
+    /*
     public boolean dfs(int[][] graph,int i,int[] vis,boolean group){
         vis[i] = (group==true)? -1 : 1;
         for(int n : graph[i])
@@ -24,5 +25,36 @@ class Solution {
             if(vis[i]==0 && !dfs(graph,i,vis,true)) return false;
         }
         return true;   
+    }
+    */
+
+
+    //bfs
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] vis = new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            if(vis[i]!=0) continue;
+            vis[i]=-1;
+            Deque<Integer> q = new ArrayDeque<>();
+            q.addLast(i);
+            while(q.size()>0)
+            {
+                    int node = q.removeFirst();
+                    for(int neighbour : graph[node])
+                    {
+                        if(vis[neighbour]==0) 
+                        {
+                            vis[neighbour] = (vis[node]==-1)?1:-1;
+                            q.addLast(neighbour);
+                        }
+                        else if(vis[neighbour]==vis[node]) return false;
+                    }
+            } 
+        }
+         
+        return true; 
     }
 }
