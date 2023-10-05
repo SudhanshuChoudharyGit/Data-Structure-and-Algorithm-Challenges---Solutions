9156ccf910fd96376abe4d02454f6709e4e6302d
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-10-05 11:01:00 +0530

    Time: 3 ms (60.06%) | Memory: 42.9 MB (77.50%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -1,10 +1,32 @@
 class Solution {
+
+    public int binarySearch(int target,int[] nums,int l,int r)
+    {
+        int ind = -1;
+        while(l<=r)
+        {
+            int m = l + (r-l)/2;
+            if(nums[m]<=target)
+            {
+                ind = m;
+                l = m+1;
+            }
+            else r = m-1;
+        }
+        if(ind==-1) return -1;
+        return (nums[ind]==target) ? ind : -1;
+    }
+
     public int findFinalValue(int[] nums, int original) {
         Arrays.sort(nums);
-        for(int x : nums)
+        int l = 0;
+        int r = nums.length-1;
+        int ind = binarySearch(original,nums,l,r);
+        while(ind!=-1)
         {
-            if(x>original) return original;
-            if(x == original) original*=2;
+            original*=2;
+            l = ind+1;
+            ind = binarySearch(original,nums,l,r);
         }
         return original;
     }
