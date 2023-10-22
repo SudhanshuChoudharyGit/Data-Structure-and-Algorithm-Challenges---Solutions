3278e3f03660123bbd3c50dbc26000bf38d37fee
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-10-22 17:37:23 +0530

    Time: 35 ms (57.16%) | Memory: 42.4 MB (69.84%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,6 +1,26 @@
+/*class Pair{
+    int first;
+    int second;
+
+    public Pair() {
+        first = 0;
+        second = 0;
+    }
+
+    public Pair(int x,int y) {
+        first = x;
+        second = y;
+    }
+}
+*/
+
+
 class Solution {
 
 
+
+    //dp
+    /*
     public int numSquares(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,n+1);
@@ -14,6 +34,50 @@ class Solution {
             }
         }
 
+        return dp[n];
+    }
+    */
+
+    //bfs
+    /*
+    public int numSquares(int n) {
+        Queue<Pair> q = new LinkedList<Pair>();
+        int ans = 0;
+        q.add(new Pair(0,1));
+        while(q.size()>0)
+        {
+            int levelSize = q.size();
+            while(levelSize>0)
+            {
+                Pair p = q.poll();
+                int sum = p.first;
+                int index = p.second;
+                if(sum==n) return ans;
+                for(int i=index;i*i<=n;i++)
+                {
+                    if(sum+i*i<=n) q.add(new Pair(sum+i*i,i));
+                }
+                levelSize--;
+            }
+            ans++;
+        }
+
+        return ans;
+
+    }*/
+
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=1;j*j<=i;j++)
+            {
+                dp[i] = Math.min(dp[i],dp[i-j*j]+1);
+            }
+        }
         return dp[n];
     }
 }
