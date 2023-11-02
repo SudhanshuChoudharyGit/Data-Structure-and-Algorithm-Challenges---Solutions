9166177b0d71e406cc8b33f91cfa8a4a3cff3ad4
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-11-02 11:40:38 +0530

    Time: 5 ms (65.11%) | Memory: 65.3 MB (45.55%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -9,6 +9,48 @@
  * }
  */
 class Solution {
+
+    public ListNode reverseLinkedList(ListNode node){
+        ListNode prev= null;
+        ListNode curr = node;
+        ListNode nxt = node.next;
+        while(nxt!=null)
+        {
+            curr.next = prev;
+            prev= curr;
+            curr=nxt;
+            nxt= nxt.next;
+        }
+        curr.next = prev;
+        return curr;
+    }
+
+    //reverse LL solution
+    public int pairSum(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode revList = reverseLinkedList(slow.next);
+        int maxSum = 0;
+        ListNode temp1 = head;
+        ListNode temp2 = revList;
+        while(temp1!=null && temp2!=null)
+        {
+            maxSum = Math.max(maxSum,temp1.val+temp2.val);
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        return maxSum;
+    }
+
+
+
+    //stack solution
+    /*
     public int pairSum(ListNode head) {
         ListNode slow = head;
         ListNode fast = head.next;
@@ -30,5 +72,6 @@ class Solution {
         }
         return maxSum;
     }
+    */
 
 }
