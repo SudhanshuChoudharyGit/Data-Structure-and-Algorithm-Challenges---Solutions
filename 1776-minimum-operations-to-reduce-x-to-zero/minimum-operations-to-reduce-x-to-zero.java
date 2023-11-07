3a81628eaaf0997995b8f5746d904fc86b7cff18
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-11-07 10:33:39 +0530

    Time: 5 ms (52.42%) | Memory: 61.4 MB (30.03%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        //maximum length subarray with sum = total sum - x;
+        int n = nums.length;
+        int totalSum = 0;
+        for(int ele : nums) totalSum+= ele;
+        int target = totalSum - x;
+        int l = 0;
+        int r = 0;
+        int currSum = 0;
+        int len = -1;
+        while(r<n)
+        {
+            currSum += nums[r];
+            while(l<=r && currSum>target)
+            {
+                currSum -= nums[l];
+                l++;
+            }
+            if(currSum==target) len = Math.max(len,r-l+1);
+            r++;
+        }
+        if(len==-1) return -1;
+        return n-len;
+    }
+}
