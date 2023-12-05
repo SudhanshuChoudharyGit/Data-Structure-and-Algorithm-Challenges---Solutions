689b35e0c7d9573b478cfea2c61598333eead2de
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-12-05 09:48:46 +0530

    Time: 6 ms (49.00%) | Memory: 43.6 MB (85.42%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,22 +1,29 @@
 class Solution {
     public String convert(String s, int numRows) {
-        int len = s.length();
+    int n = s.length();
+    StringBuffer[] array = new StringBuffer[numRows];
+    for(int i=0;i<numRows;i++) array[i] = new StringBuffer();
 
-        StringBuffer[] sbrArray = new StringBuffer[numRows];
-        for(int i=0;i<numRows;i++) sbrArray[i] = new StringBuffer();
-
-        int i = 0;
-        while(i<len)
+    int i=0;
+    while(i<n)
+    {
+        //vertically downward
+        for(int ind=0;ind<numRows && i<n;ind++)
         {
-            for(int ind=0;ind<numRows && i<len;ind++)
-            sbrArray[ind].append(s.charAt(i++));
-            for(int ind=numRows-2;ind>=1 && i<len;ind--)
-            sbrArray[ind].append(s.charAt(i++));
+            array[ind].append(s.charAt(i++));
         }
 
-        StringBuffer ans = new StringBuffer();
-        for(StringBuffer curr : sbrArray) ans.append(curr);
+        //bent upward
+        for(int ind=numRows-2;ind>=1 && i<n;ind--)
+        {
+            array[ind].append(s.charAt(i++));
+        }
+    }
 
-        return ans.toString();
+    StringBuffer ans = new StringBuffer();
+    for(StringBuffer curr : array) ans.append(curr);
+
+    return ans.toString();
+
     }
 }
