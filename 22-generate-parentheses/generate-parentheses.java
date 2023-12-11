9a24ba52de494e89637ac4b962a66951fa400af3
Sudhanshu Choudhary <130995829+SudhanshuChoudharyGit@users.noreply.github.com>
2023-12-11 11:31:27 +0530

    Time: 1 ms (88.70%) | Memory: 43.8 MB (21.92%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,13 +1,26 @@
 class Solution {
 
-    public void helper(int open,int close, String curr, int n,List<String> ans) {
+    //recursive function
+    public void helper(int open,int close,String curr,List<String> ans,int n) {
+        //base case (ending condition)
         if(curr.length()==2*n)
         {
             ans.add(curr);
             return;
         }
-        if(open<n) helper(open+1,close,curr+"(",n,ans);
-        if(close<open) helper(open,close+1,curr+")",n,ans);
+
+       //open use
+        if(open<n)
+        {
+            helper(open+1,close,curr+"(",ans,n);
+        }
+
+        //close use
+        if(open>close)
+        {
+            helper(open,close+1,curr+")",ans,n);
+        }
+        
     }
 
     public List<String> generateParenthesis(int n) {
@@ -15,7 +28,7 @@ class Solution {
         int open = 0;
         int close = 0;
         String curr = "";
-        helper(open,close,curr,n,ans);
+        helper(open,close,curr,ans,n);
         return ans;
     }
 }
